e56bf31620cf73a4ba20a3cd2d71d879ac90e71f
hdfiresky <hdfiresky@gmail.com>
2025-08-15 23:09:10 +0530

    feat: root launch launchpad

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: "/",
+    base: "/launchpad/",
     define: {
       "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
@@ -35,17 +35,17 @@ export default defineConfig(({ mode }) => {
           theme_color: "#ffffff",
           icons: [
             {
-              src: "/pwa-192x192.png",
+              src: "launchpad/pwa-192x192.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "/pwa-512x512.png",
+              src: "launchpad/pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
             },
             {
-              src: "/pwa-512x512.png",
+              src: "launchpad/pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "any maskable",
