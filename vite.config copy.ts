ae384d12adb76faa2ddb931fc8b2413066016af2
hdfiresky <hdfiresky@gmail.com>
2025-08-15 18:54:59 +0530

    feat: making web app pwa

--- vite.config copy.ts
+++ vite.config copy.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: "/",
+    base: "/asset-generator/",
     define: {
       "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
@@ -26,26 +26,26 @@ export default defineConfig(({ mode }) => {
           "pwa-512x512.png",
         ],
         manifest: {
-          name: "Launchpad",
-          short_name: "Launch",
-          start_url: "/",
-          scope: "/",
+          name: "Asset generator",
+          short_name: "AssetGen",
+          start_url: "/asset-generator/",
+          scope: "/asset-generator/",
           display: "standalone",
           background_color: "#ffffff",
           theme_color: "#ffffff",
           icons: [
             {
-              src: "/pwa-192x192.png",
+              src: "/asset-generator/pwa-192x192.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "/pwa-512x512.png",
+              src: "/asset-generator/pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
             },
             {
-              src: "/pwa-512x512.png",
+              src: "/asset-generator/pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "any maskable",
