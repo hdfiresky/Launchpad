7bc64711cfd480e474c0228a16a87e1a756b67f9
hdfiresky <hdfiresky@gmail.com>
2025-08-16 19:14:34 +0530

    fix: path patch

--- vite.config.ts
+++ vite.config.ts
@@ -35,17 +35,17 @@ export default defineConfig(({ mode }) => {
           theme_color: "#ffffff",
           icons: [
             {
-              src: "launchpad/pwa-192x192.png",
+              src: "/launchpad/pwa-192x192.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "launchpad/pwa-512x512.png",
+              src: "/launchpad/pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
             },
             {
-              src: "launchpad/pwa-512x512.png",
+              src: "/launchpad/pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "any maskable",
