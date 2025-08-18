e405d6d7dede2dc55d323f555bb6fb7123d18844
hdfiresky <hdfiresky@gmail.com>
2025-08-18 10:20:15 +0530

    fix: brand icon

--- config.ts
+++ config.ts
@@ -8,7 +8,7 @@ import { BASE_URL } from "./constants";
  * The path to the main brand icon used in the header and as the favicon.
  * It's defined here to be easily configurable.
  */
-export const BRAND_ICON_URL = BASE_URL + '/launchpad/pwa-512x512.png';
+export const BRAND_ICON_URL = BASE_URL + '/launchpad/pwa-512x512.webp';
 
 
 
