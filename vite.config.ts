0ee07a2b3d436482a02a992288dc4f7d76b89cc7
hdfiresky <hdfiresky@gmail.com>
2025-08-18 11:59:17 +0530

    fix: url fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    base: "/",
+    base: "/launchpad/",
     define: {
       "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
       "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
